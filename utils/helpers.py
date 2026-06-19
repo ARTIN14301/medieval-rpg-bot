b72c483fc66b80c81a8f0a8071dce78c7516a9ba
ARTIN14301 <artin112raider@gmail.com>
2026-06-20 00:31:23 +0330

    Refactor user power calculation and add speed/defense

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,134 +3,80 @@
 # توابع کمکی
 # ============================================
 
-import random
-import re
-from datetime import datetime, timedelta
-from utils.constants import get_title, ITEMS, ARMIES, CLASSES
-
-def calculate_power(user):
-    """محاسبه قدرت کل کاربر"""
-    power = 0
-    
-    # بونوس کلاس
-    class_bonus = CLASSES.get(user.class_name, {}).get("bonus", {}).get("power", 0)
-    power += class_bonus
-    
-    # تجهیزات
-    if user.current_weapon:
-        power += ITEMS.get(user.current_weapon, {}).get("effect", 0)
-    if user.current_armor:
-        power += ITEMS.get(user.current_armor, {}).get("effect", 0)
-    if user.current_horse:
-        power += ITEMS.get(user.current_horse, {}).get("effect", 0)
-    
-    # بونوس ارتش
-    if user.army:
-        power += ARMIES.get(user.army, {}).get("bonus", 0)
-    
-    # بونوس لول (هر لول ۱ قدرت)
-    power += user.level
-    
-    return power
-
-def calculate_exp_needed(level):
-    """محاسبه تجربه مورد نیاز برای لول بعدی"""
-    return int(100 * (1.2 ** (level - 1)))
-
-def check_cooldown(last_time, cooldown_seconds):
-    """بررسی کول‌داون"""
-    if not last_time:
-        return True, 0
-    elapsed = (datetime.utcnow() - last_time).total_seconds()
-    if elapsed >= cooldown_seconds:
-        return True, 0
-    return False, cooldown_seconds - elapsed
-
-def paginate(items, page, per_page=5):
-    """صفحه‌بندی لیست"""
-    start = page * per_page
-    end = start + per_page
-    return items[start:end], (len(items) + per_page - 1) // per_page
-
-def get_class_emoji(class_name):
-    """دریافت ایموجی کلاس"""
-    return CLASSES.get(class_name, {}).get("emoji", "❓")
-
-def format_number(num):
-    """فرمت‌بندی اعداد با کاما"""
-    return f"{num:,}".replace(",", "٬")
-
-def get_user_display_name(user):
-    """دریافت نام نمایشی کاربر با کلاس"""
-    emoji = get_class_emoji(user.class_name)
-    return f"{emoji} {user.username}"
-
-def calculate_fight_risk(user_power, enemy_power, base_risk):
-    """محاسبه شانس شکست در جنگ"""
-    total_power = user_power + enemy_power
-    if total_power == 0:
-        return base_risk
-    reduction = int((user_power / total_power) * 100)
-    risk = max(5, min(95, base_risk - reduction))
-    return risk
-
-def get_random_quests(count=3):
-    """دریافت کوئست‌های تصادفی"""
-    from utils.constants import QUESTS
-    return random.sample(QUESTS, min(count, len(QUESTS)))
-
-
-# utils/helpers.py
-# ============================================
-# توابع کمکی
-# ============================================
-
-
-
-def escape_markdown(text: str) -> str:
-    """
-    Escape کردن همه کاراکترهای خاص MarkdownV2
-    """
-    # لیست کامل کاراکترهای خاص
-    special_chars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
-    
-    for char in special_chars:
-        text = text.replace(char, f'\\{char}')
-    
-    return text
-
+from utils.constants import CLASSES, ARMIES, ITEMS
 
 def get_user_power(user):
-    """
-    محاسبه قدرت کل کاربر
-    """
-    from utils.constants import CLASSES, ARMIES, ITEMS
+    """محاسبه قدرت کل کاربر"""
+    power = user.level * 2
     
-    power = 0
-    
-    # قدرت پایه از لول (هر لول ۲ قدرت)
-    power += user.level * 2
-    
-    # بونوس کلاس
+    # کلاس
     cls = CLASSES.get(user.class_name, {})
     power += cls.get("bonus", {}).get("power", 0)
     
-    # بونوس ارتش
+    # ارتش دیفالت
     if user.army:
         army = ARMIES.get(user.army, {})
-        power += army.get("bonus", 0)
+        army_bonus = army.get("bonus", {})
+        power += army_bonus.get("power", 0)
     
     # تجهیزات
     if user.current_weapon:
         item = ITEMS.get(user.current_weapon, {})
-        power += item.get("effect", 0)
-    
+        power += item.get("power", 0)
     if user.current_armor:
         item = ITEMS.get(user.current_armor, {})
-        power += item.get("effect", 0)
-    
+        power += item.get("power", 0)
     if user.current_horse:
         item = ITEMS.get(user.current_horse, {})
-        power += item.get("effect", 0)
+        power += item.get("power", 0)
     
     return power
+
+def get_user_speed(user):
+    """محاسبه سرعت کاربر"""
+    speed = 0
+    
+    # کلاس
+    cls = CLASSES.get(user.class_name, {})
+    speed += cls.get("bonus", {}).get("speed", 0)
+    
+    # ارتش
+    if user.army:
+        army = ARMIES.get(user.army, {})
+        army_bonus = army.get("bonus", {})
+        speed += army_bonus.get("speed", 0)
+    
+    # اسب
+    if user.current_horse:
+        item = ITEMS.get(user.current_horse, {})
+        speed += item.get("speed", 0)
+    
+    return speed
+
+def get_user_defense(user):
+    """محاسبه دفاع کاربر"""
+    defense = 0
+    
+    # کلاس
+    cls = CLASSES.get(user.class_name, {})
+    defense += cls.get("bonus", {}).get("defense", 0)
+    
+    # ارتش
+    if user.army:
+        army = ARMIES.get(user.army, {})
+        army_bonus = army.get("bonus", {})
+        defense += army_bonus.get("defense", 0)
+    
+    # زره
+    if user.current_armor:
+        item = ITEMS.get(user.current_armor, {})
+        defense += item.get("defense", 0)
+    
+    return defense
+
+def calculate_cooldown(base_cooldown, user):
+    """محاسبه کول‌داون نهایی با اعمال سرعت"""
+    speed = get_user_speed(user)
+    reduction = min(50, speed)
+    final_cooldown = base_cooldown * (1 - reduction / 100)
+    return max(300, int(final_cooldown))
